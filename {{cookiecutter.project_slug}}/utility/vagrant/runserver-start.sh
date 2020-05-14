1b6e1937cf845ea601087ff47e25f95169a57df5
Tim Santor <tsantor@xstudiosinc.com>
2020-05-14 16:40:16 -0400

    remmove Universal font

--- {{cookiecutter.project_slug}}/utility/vagrant/runserver-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /home/vagrant/env/bin/activate
-cd /home/vagrant/project
-./manage.py runserver_plus 0.0.0.0:8000
--- {{cookiecutter.project_slug}}/utility/vagrant/start-your-engines.sh
+++ {{cookiecutter.project_slug}}/utility/vagrant/start-your-engines.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install requirements:
-echo "=> Install requirements ..."
+echo "$(tput setaf 6)=> Install requirements ...$(tput sgr0)"
 pip install -r requirements/local.txt
 pip install -r requirements/production.txt
 
@@ -9,17 +9,17 @@ pip install -r requirements/production.txt
 cp env.example .env
 
 # Migrate the database:
-echo "=> Migrate ..."
+echo "$(tput setaf 6)=> Migrate ...$(tput sgr0)"
 ./manage.py migrate
 
 ## Install npm
-echo "=> Install npm and packages ..."
+echo "$(tput setaf 6)=> Install npm and packages ...$(tput sgr0)"
 sudo apt -y install npm
 npm install
 
-echo "=> Create a Superuser ..."
+echo "$(tput setaf 6)=> Create a Superuser ...$(tput sgr0)"
 ./manage.py createsuperuser
 
 ## Run the development server
-echo "=> Run the dev server ..."
+echo "$(tput setaf 6)=> Run the dev server ...$(tput sgr0)"
 npm run dev
