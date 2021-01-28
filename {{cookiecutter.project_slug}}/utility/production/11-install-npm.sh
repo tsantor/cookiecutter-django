bef3a83f3b5bc50d854cdd71cd7128b83a26a0e4
Tim Santor <tsantor@xstudiosinc.com>
2021-01-28 18:56:21 -0500

    update

--- {{cookiecutter.project_slug}}/utility/production/11-install-npm.sh
+++ {{cookiecutter.project_slug}}/utility/production/11-install-npm.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 echo '==> Install npm...'
-sudo apt-get install -y npm
+curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install -y nodejs
+
+# In order for some npm packages to work (those that require compiling code from source)
+sudo apt install build-essential
 
 cd ~/project/
 npm install
