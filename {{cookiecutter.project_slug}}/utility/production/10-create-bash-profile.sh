8b7c1a6863bfcd8395f433ad2a535e4da81e7b47
Tim Santor <tsantor@xstudiosinc.com>
2021-01-31 17:06:44 -0500

    remove vagrant stragglers

--- {{cookiecutter.project_slug}}/utility/production/10-create-bash-profile.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-touch ~/.bash_profile
-
-echo 'alias app="source ~/{{cookiecutter.project_slug}}_env/bin/activate && cd ~/{{cookiecutter.project_slug}}"' >> ~/.bash_profile
-echo 'alias collectstatic="app && ./manage.py collectstatic --settings config.settings.production"' >> ~/.bash_profile
-source ~/.bash_profile
