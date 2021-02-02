25cdcc8718d97d6fbf5be8002ad6c5fc376e24f4
Tim Santor <tsantor@xstudiosinc.com>
2021-02-02 17:44:08 -0500

    inspinia theme included for custom admin, but not activated by default

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -16,7 +16,7 @@ admin.site.site_header = "{{ cookiecutter.project_name }}"
 admin.site.index_title = "Site administration"
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/index.html"), name="home"),
+    path("", TemplateView.as_view(template_name="pages/home_inspinia.html"), name="home"),
     # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
